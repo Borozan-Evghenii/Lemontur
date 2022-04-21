bf44730fbb65ae2a4c0949d2f0143ad40a6416b2
Borozan-Evghenii <borozan.eugen@gmail.com>
2022-04-21 14:54:36 +0300

    first settings

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+
+
+
+</body>
+
+<?php wp_footer(); ?>
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#4285f4">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LemonTur</title>
+    <?php 
+    wp_head(); 
+    ?>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+    
+</head>
+<body>
+    
